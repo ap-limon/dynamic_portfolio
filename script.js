23c8a5d928997aa52f56455be06584c18a969553
ap-limon <jnsapl5@gmail.com>
2024-09-13 15:32:53 +0600

    hopefuly done with the navigation bar lets what come next

---
+++ script.js
@@ -0,0 +1,19 @@
+//smooth scrolling for navigation links
+// document.querySelector('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener("click", (e) => {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute("href")).scrollIntoView({
+//             behavior: "smooth"
+//         });
+//     });
+// });
+
+//mobile menu toggle
+const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+mobileMenuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    console.log("you got me bro")
+})
