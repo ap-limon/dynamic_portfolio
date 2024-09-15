6031f9a39fb29426cbf3af4fce72fce210f93bb0
ap-limon <jnsapl5@gmail.com>
2024-09-15 23:28:45 +0600

    done with the about me section how it looks fine

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//smooth scrolling for navigation links
-// document.querySelector('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener("click", (e) => {
-//         e.preventDefault();
+// smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
 
-//         document.querySelector(this.getAttribute("href")).scrollIntoView({
-//             behavior: "smooth"
-//         });
-//     });
-// });
+        document.querySelector(e.target.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
 
 //mobile menu toggle
 const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
